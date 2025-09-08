8ba936e2649b520aa4926beca9f98202f3b08312
unknown <richard@vanirinstalledsales.com>
2025-09-08 07:25:26 -0400

    live table

--- review.js
+++ review.js
@@ -2,7 +2,7 @@
    CONFIG / CONSTANTS
 ========================= */
 const AIRTABLE_API_KEY = "pat6QyOfQCQ9InhK4.4b944a38ad4c503a6edd9361b2a6c1e7f02f216ff05605f7690d3adb12c94a3c";
-const BASE_ID = "appzjTeapVOPpVg87";
+const BASE_ID = "appQDdkj6ydqUaUkE";
 const TABLE_ID = "tblg98QfBxRd6uivq";
 
 // Linked tables
